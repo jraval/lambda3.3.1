2fed0b54380fe308b110e4192eb3359599ed3417
John Hurdle <john.renken@lambdaschool.com>
2021-04-14 21:05:53 -0600

    added sqlite db and updated path to db

--- lambda331/app.py
+++ lambda331/app.py
@@ -10,7 +10,7 @@ def create_app():
 
     app = Flask(__name__)
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///lambda331/db.sqlite3'
     DB.init_app(app)
 
     @app.route('/')
